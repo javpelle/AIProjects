fce4b8fc72adb72804891eef72b66d788365c8df
Javier Pellejero <javier_pellejer@hotmail.com>
2018-02-26 20:18:19 +0100

    Casi hecha la parte 2

--- Práctica 6/relojes.pl
+++ Práctica 6/relojes.pl
@@ -1,16 +1,20 @@
-objetivo(Estado(_,3,_)).
-objetivo(Estado(_,8,_)).
-objetivo(Estado(3,_,_)).
-objetivo(Estado(4,_,_)).
+objetivo(estado(_,3,_)).
+objetivo(estado(_,8,_)).
+objetivo(estado(3,_,_)).
+objetivo(estado(4,_,_)).
 
-consulta() :- puede(Estado(7,11,0), objetivo,[inicial], [giraSiete, giraOnce, caer]).
-
-puede(Estado, Estado,_, []).
-puede(EstadoX, EstadoY, Visitados, [Operador|Operadores]) :-
-	movimiento(EstadoX, Estadoi, Operador),
-	\+ member(Estadoi, Visitados), puede(Estadoi, EstadoY, [Estadoi|Visitados], Operadores).
+inicial(estado(7,11,0)).
 	
-movimiento(EstadoX(X1,Y1,T), Estadoi(X2,Y2,T), giraSiete) :- is X2 (7 - X1), is Y2 Y1.
-movimiento(EstadoX(X1,Y1,T), Estadoi(X2,Y2,T), giraOnce) :- is X2 X1, is Y2 (11 - Y1).
-movimiento(EstadoX(X1,Y1,T1), Estadoi(X2,Y2,T2), caer) :- (7 - X1) >= (11 - X2), Y2 is 11, Y1 is (11 - X2 + X1), T2 is (T1 + 11 - X2).
-movimiento(EstadoX(X1,Y1,T1), Estadoi(X2,Y2,T2), caer) :- (7 - X1) < (11 - X2), Y1 is 7,Y2 is (7 - X1 + X2), T2 is (T1 + 7 - X1).
+movimiento(estado(X1,Y,T), estado(X2,Y,T), giraSiete) :- X2 is (7 - X1).
+movimiento(estado(X,Y1,T), estado(X,Y2,T), giraOnce) :- Y2 is (11 - Y1).
+movimiento(estado(X1,Y1,T1), estado(X2,Y2,T2), caer) :- X1 > Y1, Y2 is 0, X2 is (X1 - Y1), T2 is (T1 + Y1).
+movimiento(estado(X1,Y1,T1), estado(X2,Y2,T2), caer) :- X1 < Y1, X2 is 0, Y2 is (Y1 - X1), T2 is (T1 + X1).
+movimiento(estado(X1,Y1,T1), estado(X2,Y2,T2), caer) :- X1 is Y1, X2 is 0,Y2 is 0, T2 is (T1 + X1).
+
+puede(Estado,_, []) :- objetivo(Estado).
+puede(Estado, Visitados, [Operador|Operadores]) :-
+	movimiento(Estado, Estado2, Operador),
+	\+ member(Estado, Visitados),
+	puede(Estado2, [Estado2|Visitados], Operadores).
+	
+consulta() :- inicial(Estado),puede(Estado,[Estado], Operadores), write(Operadores).
