c6e9f76d2cd3fce8ed87bf5739842b5b3c715a21
Javier Pellejero <javier_pellejer@hotmail.com>
2018-02-26 21:17:57 +0100

    Parte 2 hecha

--- Práctica 6/relojes.pl
+++ Práctica 6/relojes.pl
@@ -1,20 +1,33 @@
-objetivo(estado(_,3,_)).
-objetivo(estado(_,8,_)).
-objetivo(estado(3,_,_)).
-objetivo(estado(4,_,_)).
+% Representación de los estados del problema:
+% estado(minutosParteArribaSiete, minutosParteArribaOnce)
+% donde minutosParteArribaSiete es la cantidad de arena en minutos en la parte de arriba del reloj de 7.
+%       minutosParteArribaOnce  es la cantidad de arena en minutos en la parte de arriba del reloj de 11.
 
-inicial(estado(7,11,0)).
-	
-movimiento(estado(X1,Y,T), estado(X2,Y,T), giraSiete) :- X2 is (7 - X1).
-movimiento(estado(X,Y1,T), estado(X,Y2,T), giraOnce) :- Y2 is (11 - Y1).
-movimiento(estado(X1,Y1,T1), estado(X2,Y2,T2), caer) :- X1 > Y1, Y2 is 0, X2 is (X1 - Y1), T2 is (T1 + Y1).
-movimiento(estado(X1,Y1,T1), estado(X2,Y2,T2), caer) :- X1 < Y1, X2 is 0, Y2 is (Y1 - X1), T2 is (T1 + X1).
-movimiento(estado(X1,Y1,T1), estado(X2,Y2,T2), caer) :- X1 is Y1, X2 is 0,Y2 is 0, T2 is (T1 + X1).
+% Estados Objetivo. Se considera que haya 3 minutos en cualquiera de las dos partes de cualquiera de los dos relojes.
+objetivo(estado(_,3)).
+objetivo(estado(_,8)).
+objetivo(estado(3,_)).
+objetivo(estado(4,_)).
 
-puede(Estado,_, []) :- objetivo(Estado).
-puede(Estado, Visitados, [Operador|Operadores]) :-
-	movimiento(Estado, Estado2, Operador),
-	\+ member(Estado, Visitados),
-	puede(Estado2, [Estado2|Visitados], Operadores).
+% Estado inicial. Toda la arena en las partes de abajo de ambos relojes.
+inicial(estado(0,0)).
+
+% Definicion de posibles operadores.
+movimiento(estado(X1,Y), estado(X2,Y), giraSiete, 0) :- X2 is (7 - X1).
+movimiento(estado(X,Y1), estado(X,Y2), giraOnce, 0) :- Y2 is (11 - Y1).
+movimiento(estado(X1,Y1), estado(X2,Y2), caer, Y1) :- X1 > Y1, Y2 is 0, X2 is (X1 - Y1).
+movimiento(estado(X1,Y1), estado(X2,Y2), caer, X1) :- X1 < Y1, X2 is 0, Y2 is (Y1 - X1).
+movimiento(estado(X1,Y1), estado(X2,Y2), caer, X1) :- X1 is Y1, X2 is 0,Y2 is 0.
+
+% Busqueda de la secuencia de operadores.
+puede(Estado,_, [],0) :- objetivo(Estado).
+puede(Estado, Visitados, [Operador|Operadores], T) :-
+	movimiento(Estado, Estado2, Operador, T2),
+	\+ member(Estado2, Visitados),
+	puede(Estado2, [Estado2|Visitados], Operadores, T1),
+	T is (T1 + T2).
 	
-consulta() :- inicial(Estado),puede(Estado,[Estado], Operadores), write(Operadores).
+% Consulta e impresion de los resultados.	
+consulta() :- inicial(Estado),puede(Estado,[Estado], Operadores, Tiempo),
+write('SOLUCION ENCONTRADA sin repeticion de estados: '), nl, write(Operadores), nl,
+write('TIEMPO DE LA SOLUCION: '), write(Tiempo), write(' MINUTOS.').
