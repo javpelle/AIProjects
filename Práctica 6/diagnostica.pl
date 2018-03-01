4fe4ad86b22a818db22582c2fc4a752a423b070d
ZHAOYANNI <zhni@ucm.es>
2018-03-01 14:01:12 +0100

    falta la parte de alergia

--- Práctica 6/diagnostica.pl
+++ Práctica 6/diagnostica.pl
@@ -4,27 +4,6 @@ afirmativa(si).
 negativa(n).
 negativa(no).
 
-
-tipoComponente(tripolidina, antihestaminico).
-tipoComponente(ebastina, antihestaminico).
-tipoComponente(amoxicilina, antibiotico).
-tipoComponente(ampicilina, antibiotico).
-tipoComponente(paracetamol, analgesico).
-tipoComponente('acido acetilsalicilico', analgesico).
-tipoComponente(metoclopramida, antiemetico).
-
-contiene(aspirina, 'acido acetilsalicilico').
-contiene(iniston, tripolidina).
-contiene(clamoxil, amoxicilina).
-contiene(gelocatil, paracetamol).
-contiene(ebastel, ebastina).
-contiene(britapen, ampicilina).
-contiene(primperan, metoclopramida).
-
-tipoEnfermedad(rinitis, alergia).
-tipoEnfermedad(faringitis, infeccion).
-tipoEnfermedad(meningitis, infeccion).
-
 sintoma(nauseas):- pregunta_si(nauseas).
 sintoma(vomito):- pregunta_si(vomito).
 sintoma('picor nariz'):- pregunta_si('picor nariz').
@@ -57,14 +36,37 @@ confirma(C, R) :- \+preguntado(C, _), nl, pregunta(C), read(R), asserta(pregunta
 respuesta_positiva(_, R) :- afirmativa(R).
 respuesta_positiva(C, R) :- \+afirmativa(R), \+negativa(R), write('Por favor, conteste si/s o no/n y termine con .: '), read(R2), retract(preguntado(C, R)),
 							asserta(preguntado(C, R2)), respuesta_positiva(C, R2).
+% alergico(ampicilina).
+alergico(paracetamol).
+
+tipoComponente(tripolidina, antihistaminico).
+tipoComponente(ebastina, antihistaminico).
+tipoComponente(amoxicilina, antibiotico).
+tipoComponente(ampicilina, antibiotico).
+tipoComponente(paracetamol, analgesico).
+tipoComponente('acido acetilsalicilico', analgesico).
+tipoComponente(metoclopramida, antiemetico).
+
+contiene(aspirina, 'acido acetilsalicilico').
+contiene(iniston, tripolidina).
+contiene(clamoxil, amoxicilina).
+contiene(gelocatil, paracetamol).
+contiene(ebastel, ebastina).
+contiene(britapen, ampicilina).
+contiene(primperan, metoclopramida).
+
+tipoEnfermedad(rinitis, alergia).
+tipoEnfermedad(faringitis, infeccion).
+tipoEnfermedad(meningitis, infeccion).
 
 sintomaEnfermedad(rinitis) :- sintoma('picor nariz'), sintoma('congestion nasal'), sintoma('congestion ocular').
 sintomaEnfermedad(faringitis) :- sintoma('dolor de garganta'), sintoma(fiebre), sintoma(malestar).
-sintomaEnfermedad(meningitis) :- sintoma(fiebre), sintoma('dolor de cabeza'), sintoma('rigdez en la nuca'), sintoma(nauseas).
-sintomaEnfermedad(meningitis) :- sintoma(fiebre), sintoma('dolor de cabeza'), sintoma('rigdez en la nuca'), sintoma(vomitos).
+sintomaEnfermedad(meningitis) :- sintoma(fiebre), sintoma('dolor de cabeza'), sintoma('rigidez en la nuca'), sintoma(nauseas).
+sintomaEnfermedad(meningitis) :- sintoma(fiebre), sintoma('dolor de cabeza'), sintoma('rigidez en la nuca'), sintoma(vomito).
 
-componenteEnfermedad(alergia, antihistaminico) :- sintomaEnfermedad(X), tipoEnfermedad(X,alergia). /*sintomaEnfermedad(rinitis).*/
-componenteEnfermedad(infeccion, antibiotico) :- sintomaEnfermedad(X), tipoEnfermedad(X,infeccion).
+% para saber qué tipo de componente quimico hay que usar para un tipo de enfermedad.
+componenteEnfermedad(Enfermedad,antihistaminico):- tipoEnfermedad(Enfermedad, alergia).
+componenteEnfermedad(Enfermedad,antibiotico):- tipoEnfermedad(Enfermedad, infeccion).
 
 sintomaComponente(analgesico):- sintoma(fiebre).
 sintomaComponente(analgesico):- sintoma('dolor de cabeza').
@@ -72,6 +74,12 @@ sintomaComponente(analgesico):- sintoma('dolor de garganta').
 sintomaComponente(antihistaminico):- sintoma('congestion nasal').
 sintomaComponente(antihistaminico):- sintoma('congestion ocular').
 
-recetar(enfermedad, medicamento):- tipoEnfermedad(enfermedad,X), componenteEnfermedad(X,Y), tipoComponente(Z,Y), contiene(medicamento,Z).
+recetar(Enfermedad):- componenteEnfermedad(Enfermedad,A),tipoComponente(Z,A), \+alergico(Z),contiene(Medicamento,Z),!,
+					write('Su enfermedad es: '),write(Enfermedad),nl,write('Te recetamos el medicamento: '), nl, write(Medicamento).
+recetar(Enfermedad):- write('Lo sentimos, no hemos podido recertarle algún medicamento. Le recomendamos que acuda a un médico.'),nl.
 
-diagnosticar():- sintomaEnfermedad(X),recetar(X,Y),write('recetamos el medicamento: '), nl, write(Y).
+aliviar():- write('Para aliviar sus sintomas, puedes tomar: '), sintomaComponente(X), tipoComponente(Z, X), \+alergico(Z), contiene(Medicamento, Z), write(Medicamento), nl.
+aliviar():- write('Lo sentimos, no hemos podido recertarle algún medicamento. Le recomendamos que acuda a un médico.'),nl.
+
+diagnosticar():- sintomaEnfermedad(X),recetar(X).
+diagnosticar():- write('Lo sentimos, no hemos podido identificar la enfermadad.'), aliviar().
